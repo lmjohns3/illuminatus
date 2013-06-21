87b287d41eafc93898c8cca30918378033726615
Leif Johnson <leif@leifjohnson.net>
2013-06-21 02:57:07 -0500

    No need to kill background jobs when script finishes.

--- main.sh
+++ main.sh
@@ -2,13 +2,6 @@
 
 # install npm packages with "npm install coffee-script stylus"
 
-node_modules/coffee-script/bin/coffee -cbw static/js &
-coffee_pid=$!
-
-node_modules/stylus/bin/stylus -c -w static/css &
-stylus_pid=$!
-
+node node_modules/coffee-script/bin/coffee -c -w -b static/js &
+node node_modules/stylus/bin/stylus -c -w static/css &
 python main.py --reload
-
-kill $stylus_pid
-kill $coffee_pid
