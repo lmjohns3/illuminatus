55edb3027b418161dda299ac220e06609eb659e4
Leif Johnson <leif@leifjohnson.net>
2013-10-13 21:51:38 -0500

    Add interface for adding photo operations.

--- lmj/photos/photos.py
+++ lmj/photos/photos.py
@@ -17,9 +17,9 @@ class Photo(object):
     def __init__(self, id=-1, path='', meta=None, exif=None, ops=None):
         self.id = id
         self.path = path
-        self.meta = meta or {}
-        self.exif = exif or {}
-        self.ops = ops or []
+        self.meta = parse(meta or '{}')
+        self.exif = parse(exif or '{}')
+        self.ops = parse(ops or '[]')
 
     @property
     def tag_set(self):
@@ -76,10 +76,21 @@ class Photo(object):
                     path=self.path,
                     stamp=self.stamp,
                     meta=self.meta,
-                    exif=self.exif,
-                    ops=self.ops,
                     thumb=self.thumb_path,
                     tags=list(self.tag_set))
+
+    def make_thumbnails(self, sizes=(('full', 1000), ('thumb', 200)), replace=False):
+        import lmj.photos
+        base = os.path.dirname(lmj.photos.DB)
+        img = self.get_image()
+        for name, size in sorted(sizes, key=lambda x: -x[1]):
+            p = os.path.join(base, name, self.thumb_path)
+            dirname = os.path.dirname(p)
+            try: os.makedirs(dirname)
+            except: pass
+            if replace or not os.path.exists(p):
+                img.thumbnail((size, size), PIL.Image.ANTIALIAS)
+                img.save(p)
 
     def get_image(self):
         img = PIL.Image.open(self.path)
@@ -91,34 +102,32 @@ class Photo(object):
             img = img.rotate(-270)
 
         for op in self.ops:
-            key = op['type']
-            if key == 'equalize_histogram':
+            key = op['key']
+            if key == 'eq':
                 # http://opencvpython.blogspot.com/2013/03/histograms-2-histogram-equalization.html
                 img = cv2.equalizeHist(img)
                 continue
-            if key == 'crop':
+            if key == 'cr':
                 x1, y1, x2, y2 = op['box']
                 width, height = img.size
-                x1 *= width
-                y1 *= height
-                x2 *= width
-                y2 *= height
+                x1 = int(width * x1)
+                y1 = int(height * y1)
+                x2 = int(width * x2)
+                y2 = int(height * y2)
                 img = img.crop([x1, y1, x2, y2])
                 continue
-            if key == 'rotate':
+            if key == 'ro':
                 img = img.rotate(op['degrees'])
+                continue
+            if key == 'cb':
+                img = img.point(op['gamma'], op['alpha'])
                 continue
             # TODO: apply more image transforms
 
         return img
 
-    def make_thumbnails(self, sizes=(('full', 1000), ('thumb', 200)), replace=False):
-        img = self.get_image()
-        for name, size in sorted(sizes, key=lambda x: -x[1]):
-            p = os.path.join(os.path.dirname(DB), name, self.thumb_path)
-            dirname = os.path.dirname(p)
-            try: os.makedirs(dirname)
-            except: pass
-            if replace or not os.path.exists(p):
-                img.thumbnail((size, size), PIL.Image.ANTIALIAS)
-                img.save(p)
+    def add_op(self, key, **op):
+        import lmj.photos
+        op['key'] = key
+        self.ops.append(op)
+        lmj.photos.update(self)
