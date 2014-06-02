0517af54407716515f5c7674250589034b7fa900
Leif Johnson <leif@leifjohnson.net>
2014-06-01 22:23:24 -0500

    Merge groups() with tags() in serving code.

--- lmj/media/commands/serve.py
+++ lmj/media/commands/serve.py
@@ -1,10 +1,12 @@
 import bottle
 import climate
 import collections
-import lmj.media
 import os
 import random
 import sys
+
+from lmj.media import db
+from lmj.media.util import parse, stringify, tag_sort_key
 
 cmd = climate.add_command('serve')
 cmd.add_argument('--host', default='localhost',
@@ -25,113 +27,73 @@ def static(path):
     return bottle.static_file(path, os.curdir)
 
 
-@bottle.get('/groups')
-def groups():
-    # build up some in-memory mappings from the database.
-    tags = {}
-    ids = collections.defaultdict(set)
-    with lmj.media.db.connect() as db:
-        tags = dict(db.execute('SELECT id, name FROM tag'))
-        for tid, pid in db.execute('SELECT tag_id, media_id FROM media_tag'):
-            ids[tid].add(pid)
-
-    # select a sample of pieces from each tag group.
-    selected = {}
-    union = set()
-    for tid, pids in ids.items():
-        s = random.sample(pids, 3) if len(pids) > 3 else list(pids)
-        selected[tid] = s
-        union |= set(s)
-    union = tuple(union)
-
-    # get metadata from the db for all selected pieces.
-    metas = {}
-    with lmj.media.db.connect() as db:
-        for a in range(0, len(union), 512):
-            unio = union[a:a+512]
-            sql = ('SELECT id, meta FROM media WHERE id IN (%s)' %
-                   ','.join('?' for _ in unio))
-            metas.update(dict(db.execute(sql, unio)))
-
-    # assemble data for each group and send it over the wire.
-    groups = []
-    for tid, pids in ids.items():
-        groups.append(dict(
-            name=tags[tid],
-            count=len(pids),
-            pieces=[dict(thumb=lmj.media.util.parse(metas[i])['thumb'],
-                         degrees=20 * random.random() - 10)
-                    for i in selected[tid]]))
-    return lmj.media.util.stringify(groups)
+@bottle.get('/tag-names')
+def tag_names():
+    return stringify([dict(name=t, key=tag_sort_key(t)) for t in db.tag_names()])
 
 
 @bottle.get('/tags')
 def tags():
     req = bottle.request
     tags = tuple(t.strip() for t in req.query.tags.split('|') if t.strip())
-    if not tags:
-        with lmj.media.db.connect() as db:
-            return lmj.media.util.stringify(
-                [t for _, t in db.execute('SELECT id, name FROM tag')])
     counts = collections.defaultdict(int)
-    for p in lmj.media.db.find_tagged(tuple(tags)):
-        for t in p.tag_set:
+    for m in db.find(tags=tags):
+        for t in m.tag_set:
             counts[t] += 1
-    return lmj.media.util.stringify(
-        [dict(name=t, count=c) for t, c in counts.items()])
+    return stringify([dict(name=t, count=c) for t, c in counts.items()])
 
 
-@bottle.get('/photo')
-def photos():
+@bottle.get('/media')
+def list_media():
     req = bottle.request
-    tags = (t.strip() for t in req.query.tags.split('|') if t.strip())
-    return lmj.media.util.stringify(
-        [p.to_dict() for p in lmj.media.db.find_tagged(
-            tuple(tags),
+    tags = tuple(t.strip() for t in req.query.tags.split('|') if t.strip())
+    return stringify(
+        [m.to_dict() for m in db.find(
+            tags=tags,
             offset=int(req.query.offset or 0),
             limit=int(req.query.limit or 10))])
 
+@bottle.get('/media/<id:int>')
+def read_media(id):
+    return stringify(db.find_one(id).to_dict())
 
-@bottle.post('/photo/<id:int>')
-def post_photo(id):
-    p = lmj.media.db.find_one(id)
-    f = lmj.media.util.parse(list(bottle.request.forms)[0])
+@bottle.put('/media/<id:int>')
+def update_media(id):
+    m = db.find_one(id)
+    f = parse(list(bottle.request.forms)[0])
     if 'meta' in f:
-        p.meta = f['meta']
-        lmj.media.db.update(p)
-    return lmj.media.util.stringify(p.to_dict())
+        m.meta = f['meta']
+        db.update(m)
+    return stringify(m.to_dict())
+
+@bottle.delete('/media/<id:int>')
+def delete_media(id):
+    m = db.find_one(id)
+    db.delete(m.id, m.path)
 
 
-@bottle.post('/photo/<id:int>/rotate')
+@bottle.post('/photos/<id:int>/rotate')
 def rotate_photo(id):
-    lmj.media.db.find_one(id).add_op(
-        'ro', degrees=float(bottle.request.forms.get('degrees')))
+    post = lambda k: float(bottle.request.forms.get(k))
+    db.find_one(id).add_op('ro', degrees=post('degrees'))
     return 'ok'
 
-@bottle.post('/photo/<id:int>/contrast')
+@bottle.post('/photos/<id:int>/contrast')
 def contrast_photo(id):
     post = lambda k: float(bottle.request.forms.get(k))
-    lmj.media.db.find_one(id).add_op(
-        'cb', gamma=post('gamma'), alpha=post('alpha'))
+    db.find_one(id).add_op('cb', gamma=post('gamma'), alpha=post('alpha'))
     return 'ok'
 
-@bottle.post('/photo/<id:int>/crop')
+@bottle.post('/photos/<id:int>/crop')
 def crop_photo(id):
     post = lambda k: float(bottle.request.forms.get(k))
-    lmj.media.db.find_one(id).add_op(
-        'cr', box=[post(k) for k in 'x1 y1 x2 y2'.split()])
+    db.find_one(id).add_op('cr', box=[post(k) for k in 'x1 y1 x2 y2'.split()])
     return 'ok'
 
-@bottle.post('/photo/<id:int>/equalize')
+@bottle.post('/photos/<id:int>/equalize')
 def equalize_photo(id):
-    lmj.media.db.find_one(id).add_op('eq')
+    db.find_one(id).add_op('eq')
     return 'ok'
-
-
-@bottle.delete('/photo/<id:int>')
-def delete_photo(id):
-    p = lmj.media.db.find_one(id)
-    lmj.media.db.delete(p.id, p.path)
 
 
 def main(args):
