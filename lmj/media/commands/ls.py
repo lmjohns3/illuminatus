805c8a0a9443d8d311d28a16d4e0b45d4e9005ba
Leif Johnson <leif@leifjohnson.net>
2014-06-01 22:28:26 -0500

    Add more command-line list filters.

--- lmj/media/commands/ls.py
+++ lmj/media/commands/ls.py
@@ -1,18 +1,42 @@
 import climate
 import datetime
+import fnmatch
 import lmj.media
 import os
 import sys
 import traceback
 
 cmd = climate.add_command('ls')
-cmd.add_argument('tag', nargs='+', metavar='TAG',
-                 help='show only photos with these TAGs')
+cmd.add_argument('-a', '--after', metavar='DATE',
+                 help='show only media from after DATE')
+cmd.add_argument('-b', '--before', metavar='DATE',
+                 help='show only media from before DATE')
+cmd.add_argument('-j', '--json', action='store_true',
+                 help='display results as JSON')
+cmd.add_argument('-p', '--path', metavar='PATH',
+                 help='show only media matching this PATH')
+cmd.add_argument('-t', '--tag', nargs='+', metavar='TAG',
+                 help='show only media with these TAGs')
 cmd.set_defaults(mod=sys.modules[__name__])
 
 logging = climate.get_logger('lmj.media.ls')
 
 
 def main(args):
-    for p in lmj.media.db.find_tagged(args.tag):
-        logging.info('%s %s', '|'.join(sorted(p.tag_set)), p.path)
+    pieces = sorted(
+        lmj.media.db.find(
+            after=args.after,
+            before=args.before,
+            path=args.path,
+            tags=args.tag,
+        ),
+        key=lambda p: p.path)
+    if args.json:
+        print(lmj.media.util.stringify([p.to_dict() for p in pieces]))
+        return
+    for p in pieces:
+        print('{}  {}  {}'.format(
+            p.stamp.strftime('%Y-%m-%dT%H:%M:%S'),
+            p.path,
+            '|'.join(lmj.media.util.sort_tags(p.tag_set)),
+        ))
