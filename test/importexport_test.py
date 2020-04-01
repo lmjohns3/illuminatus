501c74034f9661419796a2b4fc4df9b6ada9c957
Leif Johnson <leif@lmjohns3.com>
2020-04-01 12:11:26 -0700

    Mock out celery task invocation for test.

--- test/importexport_test.py
+++ test/importexport_test.py
@@ -1,10 +1,14 @@
 import illuminatus.importexport
+import illuminatus.tasks
+import unittest.mock
 
 from util import *
 
 
-def test_import_asset(sess):
+@unittest.mock.patch('illuminatus.tasks.update_from_content')
+def test_import_asset(mock, sess):
     illuminatus.importexport.maybe_import_asset(
         sess, PHOTO_PATH, tags={'uvw'}, path_tags=2)
     asset = sess.query(Asset).get(4)
-    assert asset.tags == {'uvw', 'testdata', 'test'}
+    assert asset.path == PHOTO_PATH
+    mock.delay.assert_called()
